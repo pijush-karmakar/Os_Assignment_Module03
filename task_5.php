7572208007849ef02523b66c5b794d3ab949fbd8
pijush karmakar <pijushkarmakar45@gmail.com>
2023-09-26 21:48:55 +0600

    Assignment module03

--- task_5.php
+++ task_5.php
@@ -2,30 +2,6 @@
 
 // Task 5: Password Generator
 
-// function generatePassword($length){
-
-
-
-// }
-
-// generatePassword($length);
-
-// function generatePassword($length) {
-//     $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-//     $count = mb_strlen($chars);
-
-//     for ($i = 0, $result = ''; $i < $length; $i++) {
-//         $index = rand(0, $count - 1);
-//         $result .= mb_substr($chars, $index, 1);
-//     }
-
-//     echo $result;
-// }
-
-// generatePassword(12);
-
-
-
 function generatePassword( $length ) {
     $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?"; 
     $password = substr( str_shuffle( $chars ), 0, $length );
